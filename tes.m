5f7f13d5c67de387ad9b6325bceb136c59ce9015
Muhammad Bima Zehansyah <mbimazehansyah@gmail.com>
2016-10-02 17:47:05 +0700

    Delete tes.m

--- tes.m
+++
@@ -1,8 +0,0 @@
- %%membaca serta menyimpan wefie.jpg pada variable wefie
- wefie = imread('wefie.jpg'); 
- %%merubah citra rgb ke grayscale dengan memanfaatkan variable grayscale dengan memanfaatkan variable wefie
- grayscale = uint8 (0.2989*double(wefie(: , : , 1))+0.5870*double(wefie(: , : ,1))+0.1141*double(wefie(:,:,1)));
- %%menampilkan citra grayscale dengan menggunakan function imshow
- imshow (grayscale);
- %%menampilkan histogram pada grayscale dengan function imhist dengan parameter grayscale
- imhist (grayscale);
